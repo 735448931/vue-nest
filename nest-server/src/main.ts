bcdb780d2e42c15d3ffc494cdc2442fc409aa783
HX <735448931@qq.com>
2025-11-09 19:51:41 +0800

    feat:补充

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -4,9 +4,17 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 import { HttpFilter } from './shared/globa-http.filter'
 import { ValidationPipe } from '@nestjs/common'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379
+    }
+  })
   app.enableCors()
   app.setGlobalPrefix('api')
   app.useGlobalFilters(new HttpFilter())
@@ -15,6 +23,7 @@ async function bootstrap() {
   app.useStaticAssets('public', { prefix: '/static' })
   
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
+  await app.startAllMicroservices()
 	await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
