4135b5a3d659be8a4c7f44a4e1cb6e0c3d785fa8
HX <735448931@qq.com>
2025-11-12 14:10:04 +0800

    fix:学些

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -4,6 +4,9 @@ import { isInt, IsNotEmpty, max, MaxLength } from 'class-validator'
 import { LoginGuard } from './login.guard'
 import { RequireLogin, RequirePermission } from './custom-decorator'
 import { Observable } from 'rxjs'
+import { randomUUID } from 'crypto'
+import * as qrcode from 'qrcode'
+import * as os from 'os'
 export class Ooo {
 	@MaxLength(5)
 	name: string
@@ -46,16 +49,101 @@ export class AppController {
 	// SSE数据推送
 	@Sse('stream')
 	stream() {
-		return new Observable((observer) => { 
+		return new Observable((observer) => {
 			observer.next({ data: { msg: 'aaa' } })
-			
+
 			setTimeout(() => {
 				observer.next({ data: { msg: 'bbb' } })
-			}, 2000);
+			}, 2000)
 
 			setTimeout(() => {
 				observer.next({ data: { msg: 'ccc' } })
-			}, 5000);
+			}, 5000)
 		})
+	}
+
+	@Get('qrcode/generate')
+	async generate() {
+		const uuid = randomUUID()
+		const dataUrl = await qrcode.toDataURL(
+			`http://192.168.0.3:8888/login?id=${uuid}`
+		)
+		return {
+			qrcode_id: uuid,
+			img: dataUrl
+		}
+	}
+
+	@Get('status')
+	status() {
+		return {
+			cpu: this.getCpuInfo(),
+			mem: this.getMemInfo(),
+			sys: this.getSysInfo()
+		}
+	}
+
+	bytesToGB(bytes) {
+		const gb = bytes / (1024 * 1024 * 1024)
+		return gb.toFixed(2)
+	}
+
+	getMemInfo() {
+		const totalMemory = os.totalmem()
+		const freeMemory = os.freemem()
+		const usedMemory = totalMemory - freeMemory
+		const memoryUsagePercentage = (
+			((totalMemory - freeMemory) / totalMemory) *
+			100
+		).toFixed(2)
+		const mem = {
+			total: this.bytesToGB(totalMemory),
+			used: this.bytesToGB(usedMemory),
+			free: this.bytesToGB(freeMemory),
+			usage: memoryUsagePercentage
+		}
+		return mem
+	}
+
+	getCpuInfo() {
+		const cpus = os.cpus()
+		const cpuInfo = cpus.reduce(
+			(info, cpu) => {
+				info.cpuNum += 1
+				info.user += cpu.times.user
+				info.sys += cpu.times.sys
+				info.idle += cpu.times.idle
+				info.total += cpu.times.user + cpu.times.sys + cpu.times.idle
+				return info
+			},
+			{ user: 0, sys: 0, idle: 0, total: 0, cpuNum: 0 }
+		)
+		const cpu = {
+			cpuNum: cpuInfo.cpuNum,
+			sys: ((cpuInfo.sys / cpuInfo.total) * 100).toFixed(2),
+			used: ((cpuInfo.user / cpuInfo.total) * 100).toFixed(2),
+			free: ((cpuInfo.idle / cpuInfo.total) * 100).toFixed(2)
+		}
+		return cpu
+	}
+
+	getSysInfo() {
+		return {
+			computerName: os.hostname(),
+			computerIp: this.getServerIP(),
+			osName: os.platform(),
+			osArch: os.arch()
+		}
+	}
+
+	getServerIP() {
+		const nets = os.networkInterfaces() as any
+		for (const name of Object.keys(nets)) {
+			for (const net of nets[name]) {
+				if (net.family === 'IPv4' && !net.internal) {
+					return net.address
+				}
+			}
+		}
 	}
 }
--- vue-client/src/page/login/index.vue
+++ vue-client/src/page/login/index.vue
@@ -1,11 +1,40 @@
 <template>
     <div>
-
+        <!-- <img :src="src" alt=""> -->
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 
+const src = ref('')
+
+
+// function queryStatus(id) {
+//     axios.get('http://localhost:3000//api/qrcode/check?id=' + id).then(res => {
+//         const status = res.data.status;
+
+//         let content = '';
+//         switch (status) {
+//             case 'noscan': content = '未扫码'; break;
+//             case 'scan-wait-confirm': content = '已扫码，等待确认'; break;
+//             case 'scan-confirm': content = '已确认'; break;
+//             case 'scan-cancel': content = '已取消'; break;
+//         }
+//         document.getElementById('info').textContent = content;
+
+//         if (status === 'noscan' || status === 'scan-wait-confirm') {
+//             setTimeout(() => queryStatus(id), 1000);
+//         }
+//     })
+// }
+
+
+// onMounted(async () => {
+//     const res = await axios.get('http://localhost:3000/api/qrcode/generate')
+//     src.value = res.data.img
+// })
 </script>
 
 <style scoped lang="scss">
