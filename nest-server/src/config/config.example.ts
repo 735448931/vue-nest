00047d4acc39942b2156bf7864f08db1f4041fcd
HX <735448931@qq.com>
2025-11-19 18:31:30 +0800

    修复

---
+++ nest-server/src/config/config.example.ts
@@ -0,0 +1,18 @@
+const config = {
+	ali_oss: {
+		region: '',
+		accessKeyId: '',
+		accessKeySecret: '',
+		authorizationV4: true,
+		bucket: 'hx-nest'
+	},
+	openAi: {
+		apiKey: ''
+	},
+	deepseek: {
+		apiKey: '',
+		baseURL: 'https://api.deepseek.com'
+	}
+}
+
+export default config
